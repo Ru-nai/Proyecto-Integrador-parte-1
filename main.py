b32cfa77ea76b2c1a7100dfb47c29f900c7797fd
Laura <lllauracb@gmail.com>
2023-08-30 20:29:30 -0500

    READM.me file

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 
+import curses
+
 jugador_nick = input() #Solicita el nombre del jugador
 
 print(f'¡Bienvenido, {jugador_nick}!') #Imprime mensaje de bienvenida + el nickname
+
+
+arrow_key = curses.initscr().getch()
+if arrow_key == curses.KEY_UP:
+  print("Up")
+elif arrow_key == curses.KEY_DOWN:
+  print("Down")
+elif arrow_key == curses.KEY_RIGHT:
+  print("Right")
+elif arrow_key == curses.KEY_LEFT:
+  print("Left")
+else:
+  print("Not an arrow key")
