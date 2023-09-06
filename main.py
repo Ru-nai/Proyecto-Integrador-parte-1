8c8064ef169eba01af04352d3976b8f18d866f52
Laura <lllauracb@gmail.com>
2023-09-05 19:31:03 -0500

    intento de continuar con la actividad en su segunda parte

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
+import keyboard
 
-import curses
-
-jugador_nick = input() #Solicita el nombre del jugador
-
-print(f'¡Bienvenido, {jugador_nick}!') #Imprime mensaje de bienvenida + el nickname
-
-
-arrow_key = curses.initscr().getch()
-if arrow_key == curses.KEY_UP:
-  print("Up")
-elif arrow_key == curses.KEY_DOWN:
-  print("Down")
-elif arrow_key == curses.KEY_RIGHT:
-  print("Right")
-elif arrow_key == curses.KEY_LEFT:
-  print("Left")
-else:
-  print("Not an arrow key")
+continuar = True
+while continuar:
+    keyboard.read_key() != keyboard.KEY_UP
+    print(keyboard.read_key())
+    if keyboard.is_pressed(keyboard.KEY_UP):
+        continuar == False
+        break
